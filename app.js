634cdde21d83db5ffe5a57afaa54ef7a3d43d827
samikia <kia.sami8@gmail.com>
2023-06-11 20:55:38 +0330

    fix: some error has been fixed

--- app.js
+++ app.js
@@ -23,12 +23,19 @@
   time.innerHTML = formatData(now);
   ////////////////////////////////////////////////////////////
   function currentWeather(response){
+    console.log(response);
+    celsuisTemp=Math.floor(response.data.main.temp)
   document.querySelector('#city').innerHTML=response.data.name
-  document.querySelector('#temperature').innerHTML=Math.floor(response.data.main.temp)
+  document.querySelector('#temperature').innerHTML=celsuisTemp
   document.querySelector('#wind').innerHTML=Math.round(response.data.wind.speed)
   document.querySelector('#humidity').innerHTML=response.data.main.humidity;
   document.querySelector("#description").innerHTML =
   response.data.weather[0].main;
+  document.querySelector("#icon").setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  document.querySelector("#icon").setAttribute("alt", response.data.weather[0].main);
   }
   function searchCity(city) {
     let apiKey = 'ae8a1230e3e8427c36d183396c097b5c';
@@ -51,10 +58,32 @@
     event.preventDefault();
     navigator.geolocation.getCurrentPosition(searchLocation);
   }
+  function showFornhitTemp(event){
+    event.preventDefault()
+    let farenhitTemp=(celsuisTemp*9)/5+32
+    celsuisLink.classList.remove("active")
+    fornhitLink.classList.add("active")
+  let temperatureElement= document.querySelector("#temperature")
+  temperatureElement.innerHTML=Math.round(farenhitTemp)
+
+  }
+  function showCeleciousTemp(event){
+    event.preventDefault()
+    let temperatureElement= document.querySelector("#temperature")
+    celsuisLink.classList.add("active")
+    fornhitLink.classList.remove("active")
+    temperatureElement.innerHTML=celsuisTemp
+
+  }
+  let celsuisTemp=null;
   let form=document.querySelector('#form-search')
   form.addEventListener('submit',handleSubmit)
   let currentLocationButton = document.querySelector("#current-location-button");
   currentLocationButton.addEventListener("click", getCurrentLocation);
   searchCity("New York");
+  let celsuisLink=document.querySelector("#celsius-link")
+  celsuisLink.addEventListener("click",showCeleciousTemp)
+  let fornhitLink=document.querySelector("#fornhit-link")
+  fornhitLink.addEventListener("click",showFornhitTemp)
   ///////////////////////////////////////////////////////////
   
